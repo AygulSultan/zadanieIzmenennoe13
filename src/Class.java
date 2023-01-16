aefa90e791c26001c192df8816ad4633afb3006d
AygulSultan <jakypbaevaaygul@gmail.com>
2023-01-16 13:25:59 +0300

    yu tapshyrma 13

---
+++ src/Class.java
@@ -0,0 +1,30 @@
+public class Class {
+    private int nomer;
+    private String soz;
+
+    public Class(int nomer, String soz) {
+        this.nomer = nomer;
+        this.soz = soz;
+    }
+
+
+    public int getNomer() {
+        return nomer;
+    }
+
+    public void setNomer(int nomer) {
+        this.nomer = nomer;
+    }
+
+    public String getSoz() {
+        return soz;
+    }
+
+    public void setSoz(String soz) {
+        this.soz = soz;
+    }
+}
+
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Class object= new Class(4, "soz");
+        System.out.println(object.getNomer());
+        System.out.println(object.getSoz());
+
+    }
+}
